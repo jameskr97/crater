b03cf23bc7ce3cf36e996487af4e1d0069444829
James Roberts <james@jameskr.dev>
2024-08-01 02:52:40 +0000

    Add simulator launch with ros2 compatability

--- workspace/src/crater_bot/launch/launch_sim.launch.py
+++ workspace/src/crater_bot/launch/launch_sim.launch.py
@@ -1,43 +1,116 @@
 import os
-
+import xacro
 from ament_index_python.packages import get_package_share_directory
 
-
+from pathlib import Path
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration, Command
+from launch.event_handlers import OnProcessExit
+from launch.actions import (
+    RegisterEventHandler,
+    IncludeLaunchDescription,
+    SetEnvironmentVariable,
+    DeclareLaunchArgument,
+    AppendEnvironmentVariable,
+    ExecuteProcess,
+)
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
 
 
-
 def generate_launch_description():
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
+    # Launch Arguments
+    use_sim_time = LaunchConfiguration("use_sim_time", default=True)
 
-    package_name='crater_bot' #<--- CHANGE ME
+    # Path Variables
+    crater_bot_path = os.path.join(get_package_share_directory("crater_bot"))
+    gazebo_sim_path = get_package_share_directory("ros_gz_sim")
 
-    rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    # Launch Descriptions
+    publisher_robot_state = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [os.path.join(crater_bot_path, "launch"), "/rsp.launch.py"]
+        )
     )
 
-    # Include the Gazebo launch file, provided by the gazebo_ros package
+    # Gazebo
+    gazebo_resource_path = SetEnvironmentVariable(
+        "GZ_SIM_RESOURCE_PATH", os.path.join(crater_bot_path, "worlds")
+    )
+    gazebo_arguments = LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                "gazebo_world", default_value="empty.world", description="Gz sim World"
+            ),
+        ]
+    )
     gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
-             )
+        PythonLaunchDescriptionSource(
+            [os.path.join(gazebo_sim_path, "launch"), "/gz_sim.launch.py"]
+        ),
+        launch_arguments=[
+            ("gz_args", [LaunchConfiguration("gazebo_world"), " -v4", " -r"])
+        ],
+    )
 
-    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='ros_gz_sim', executable='create',
-                        arguments=['-topic', 'robot_description',
-                                   '-entity', 'my_bot'],
-                        output='screen')
+    gz_spawn_entity = Node(
+        package="ros_gz_sim",
+        executable="create",
+        arguments=["-topic", "/robot_description", "-entity", "crater_bot"],
+        output="screen",
+    )
 
-    # Launch them all!
-    return LaunchDescription([
-        rsp,
-        gazebo,
-        spawn_entity,
-    ])
+    # ROS2 Control
+    robot_controllers = os.path.join(crater_bot_path, "config", "controllers.yaml")
+    
+    control_node = Node( package="controller_manager", executable="ros2_control_node", parameters=[
+            robot_controllers,
+        ], output="both",
+    )
 
+
+    # joint_state_broadcaster_spawner = Node(
+    #     package="controller_manager",
+    #     executable="spawner",
+    #     arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+    # )
+
+    # robot_controller_spawner = Node(
+    #     package="controller_manager",
+    #     executable="spawner",
+    #     arguments=["botwheel_explorer", "--controller-manager", "/controller_manager"],
+    # )
+
+    # delay_robot_controller_spawner_after_joint_state_broadcaster_spawner = RegisterEventHandler(
+    #     event_handler=OnProcessExit(
+    #         target_action=joint_state_broadcaster_spawner,
+    #         on_exit=[robot_controller_spawner],
+    #     )
+    # )
+
+    # ROS Visualizer
+    rviz = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+    )
+
+
+
+    res = LaunchDescription()
+    # res.add_entity(control_node)
+    # res.add_entity(joint_state_broadcaster_spawner)
+    # res.add_entity(robot_controller_spawner)
+    res.add_entity(publisher_robot_state)
+    res.add_entity(gazebo_resource_path)
+    res.add_entity(gazebo_arguments)
+    res.add_entity(gazebo)
+    res.add_entity(gz_spawn_entity)
+
+    # res.add_entity(delay_robot_controller_spawner_after_joint_state_broadcaster_spawner)
+    # res.add_entity(publisher_joint_state)
+    # res.add_entity(gz_spawn_entity)
+    # res.add_entity(rviz)
+    return res
